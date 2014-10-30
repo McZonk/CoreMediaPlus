271fd2d9f1acd257748cbc722c621b6a47da21cd
Max <mczonk@gmail.com>
2014-10-30 17:50:35 +0100

    Updated CoreMediaPlus

---
+++ CMPFormatDescription+Extensions.c
@@ -0,0 +1,6 @@
+#include "CMPFormatDescription+Extensions.h"
+
+
+const CFStringRef kCMPFormatDescriptionExtension_FrameRate = CFSTR("CMPFrameRate");
+
+const CFStringRef kCMPFormatDescriptionExtension_BitRate = CFSTR("CMPBitRate");
---
+++ CMPFormatDescription+Extensions.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <CoreMedia/CoreMedia.h>
+
+/**
+ * CFDictionary
+ */
+extern const CFStringRef kCMPFormatDescriptionExtension_FrameRate;
+
+/**
+ * CFNumber<int>
+ */
+extern const CFStringRef kCMPFormatDescriptionExtension_BitRate;
